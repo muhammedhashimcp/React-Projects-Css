1cb7275fb5bbe87849a6b3c8f5daf2bf854ee59d
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-11-22 13:07:14 +0530

    handydev movie app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import NoteContainer from './components/NoteContainer/NoteContainer.js';
-import Sidebar from './components/Sidebar/Sidebar';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Explore from './components/Explore/Explore';
+import MoviePage from './components/MoviePage/MoviePage';
 
 import './App.css';
 
 function App() {
-	const [notes, setNotes] = useState(
-		JSON.parse(localStorage.getItem('notes-app')) || []
-	);
-
-	const addNote = (color) => {
-		const tempNotes = [...notes];
-
-		tempNotes.push({
-			id: Date.now() + '' + Math.floor(Math.random() * 78),
-			text: '',
-			time: Date.now(),
-			color,
-		});
-		setNotes(tempNotes);
-	};
-
-	const deleteNote = (id) => {
-		const tempNotes = [...notes];
-
-		const index = tempNotes.findIndex((item) => item.id === id);
-		if (index < 0) return;
-
-		tempNotes.splice(index, 1);
-		setNotes(tempNotes);
-	};
-
-	const updateText = (text, id) => {
-		const tempNotes = [...notes];
-
-		const index = tempNotes.findIndex((item) => item.id === id);
-		if (index < 0) return;
-
-		tempNotes[index].text = text;
-		setNotes(tempNotes);
-	};
-
-	useEffect(() => {
-		localStorage.setItem('notes-app', JSON.stringify(notes));
-	}, [notes]);
-
 	return (
-		<div className="App">
-			<Sidebar addNote={addNote} />
-			<NoteContainer
-				notes={notes}
-				deleteNote={deleteNote}
-				updateText={updateText}
-			/>
+		<div className="app">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/movie/:movieId" element={<MoviePage />} />
+					<Route path="/explore" element={<Explore />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
